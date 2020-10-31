c0ea8d11d2008c33d27900378e2ae6db5562756e
bryanseah234 <bryanseah234@gmail.com>
2020-10-31 23:20:54 +0800

    Add files via upload

---
+++ english words.py
@@ -0,0 +1,665 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+def one():
+	one = []
+	for i in letters:
+		one.append(i)
+	return one
+
+def two():
+	two = []
+	for i in letters:
+		for j in letters:
+			word = i+j
+			two.append(word)
+	return two
+
+def three():
+	three = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				word = i+j+k
+				three.append(word)
+	return three
+
+def four():
+	four = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					word = i+j+k+l
+					four.append(word)
+	return four
+
+def five():
+	five = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						word = i+j+k+l+m
+						five.append(word)
+	return five
+
+def six():
+	six = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							word = i+j+k+l+m+n
+							six.append(word)
+	return six
+
+def seven():
+	seven = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								word = i+j+k+l+m+n+o
+								seven.append(word)
+	return seven
+
+def eight():
+	eight = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									word = i+j+k+l+m+n+o+p
+									eight.append(word)
+	return eight
+
+def nine():
+	nine = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										word = i+j+k+l+m+n+o+p+q
+										nine.append(word)
+	return nine
+
+def ten():
+	ten = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											word = i+j+k+l+m+n+o+p+q+r
+											ten.append(word)
+	return ten
+
+def eleven():
+	eleven = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												word = i+j+k+l+m+n+o+p+q+r+s
+												eleven.append(word)
+	return eleven
+
+def twelve():
+	twelve = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												for t in letters:
+													word = i+j+k+l+m+n+o+p+q+r+s+t
+													twelve.append(word)
+	return twelve
+
+def thirteen():
+	thirteen = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												for t in letters:
+													for u in letters:
+														word = i+j+k+l+m+n+o+p+q+r+s+t+u
+														thirteen.append(word)
+	return thirteen
+
+def fourteen():
+	fourteen = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												for t in letters:
+													for u in letters:
+														for v in letters:
+															word = i+j+k+l+m+n+o+p+q+r+s+t+u+v
+															fourteen.append(word)
+	return fourteen
+
+def fifthteen():
+	fifthteen = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												for t in letters:
+													for u in letters:
+														for v in letters:
+															for w in letters:
+																word = i+j+k+l+m+n+o+p+q+r+s+t+u+v+w
+																fifthteen.append(word)
+	return fifthteen
+
+def sixteen():
+	sixteen = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												for t in letters:
+													for u in letters:
+														for v in letters:
+															for w in letters:
+																for x in letters:
+																	word = i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x
+																	sixteen.append(word)
+	return sixteen
+
+def seventeen():
+	seventeen = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												for t in letters:
+													for u in letters:
+														for v in letters:
+															for w in letters:
+																for x in letters:
+																	for y in letters:
+																		word = i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y
+																		seventeen.append(word)
+	return seventeen
+
+def eightteen():
+	eightteen = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												for t in letters:
+													for u in letters:
+														for v in letters:
+															for w in letters:
+																for x in letters:
+																	for y in letters:
+																		for z in letters:
+																			word = i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z
+																			eightteen.append(word)
+	return eightteen
+
+def nineteen():
+	nineteen = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												for t in letters:
+													for u in letters:
+														for v in letters:
+															for w in letters:
+																for x in letters:
+																	for y in letters:
+																		for z in letters:
+																			for a in letters:
+																				word = i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z+a
+																				nineteen.append(word)
+	return nineteen
+
+def twenty():
+	twenty = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												for t in letters:
+													for u in letters:
+														for v in letters:
+															for w in letters:
+																for x in letters:
+																	for y in letters:
+																		for z in letters:
+																			for a in letters:
+																				for b in letters:
+																					word = i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z+a+b
+																					twenty.append(word)
+	return twenty
+
+def twentyone():
+	twentyone = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												for t in letters:
+													for u in letters:
+														for v in letters:
+															for w in letters:
+																for x in letters:
+																	for y in letters:
+																		for z in letters:
+																			for a in letters:
+																				for b in letters:
+																					for c in letters:
+																						word = i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z+a+b+c
+																						twentyone.append(word)
+	return twentyone
+
+def twentytwo():
+	twentytwo = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												for t in letters:
+													for u in letters:
+														for v in letters:
+															for w in letters:
+																for x in letters:
+																	for y in letters:
+																		for z in letters:
+																			for a in letters:
+																				for b in letters:
+																					for c in letters:
+																						for d in letters:
+																							word = i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z+a+b+c+d
+																							twentytwo.append(word)
+	return twentytwo
+
+def twentythree():
+	twentythree = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												for t in letters:
+													for u in letters:
+														for v in letters:
+															for w in letters:
+																for x in letters:
+																	for y in letters:
+																		for z in letters:
+																			for a in letters:
+																				for b in letters:
+																					for c in letters:
+																						for d in letters:
+																							for e in letters:
+																								word = i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z+a+b+c+d+e
+																								twentythree.append(word)
+	return twentythree
+
+def twentyfour():
+	twentyfour = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												for t in letters:
+													for u in letters:
+														for v in letters:
+															for w in letters:
+																for x in letters:
+																	for y in letters:
+																		for z in letters:
+																			for a in letters:
+																				for b in letters:
+																					for c in letters:
+																						for d in letters:
+																							for e in letters:
+																								for f in letters:
+																									word = i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z+a+b+c+d+e+f
+																									twentyfour.append(word)
+	return twentyfour
+
+def twentyfive():
+	twentyfive = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												for t in letters:
+													for u in letters:
+														for v in letters:
+															for w in letters:
+																for x in letters:
+																	for y in letters:
+																		for z in letters:
+																			for a in letters:
+																				for b in letters:
+																					for c in letters:
+																						for d in letters:
+																							for e in letters:
+																								for f in letters:
+																									for g in letters:
+																										word = i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z+a+b+c+d+e+f+g
+																										twentyfive.append(word)
+	return twentyfive
+
+def twentysix():
+	twentysix = []
+	for i in letters:
+		for j in letters:
+			for k in letters:
+				for l in letters:
+					for m in letters:
+						for n in letters:
+							for o in letters:
+								for p in letters:
+									for q in letters:
+										for r in letters:
+											for s in letters:
+												for t in letters:
+													for u in letters:
+														for v in letters:
+															for w in letters:
+																for x in letters:
+																	for y in letters:
+																		for z in letters:
+																			for a in letters:
+																				for b in letters:
+																					for c in letters:
+																						for d in letters:
+																							for e in letters:
+																								for f in letters:
+																									for g in letters:
+																										for h in letters:
+																											word = i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z+a+b+c+d+e+f+g+h
+																											twentysix.append(word)
+	return twentysix
+
+with open ('words.txt', 'w') as f:
+
+	one()
+	for word in one:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	two()
+	for word in two:
+		f.write(word)
+		print(word)
+		f.write("\n")
+		
+	three()
+	for word in three:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	four()
+	for word in four:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	five()
+	for word in five:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	six()
+	for word in six:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	seven()
+	for word in seven:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	eight()
+	for word in eight:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	nine()
+	for word in nine:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	ten()
+	for word in ten:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	eleven()
+	for word in eleven:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	twelve()
+	for word in twelve:
+		f.write(word)
+		print(word)
+		f.write("\n")
+		
+	thirteen()
+	for word in thirteen:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	fourteen()
+	for word in fourteen:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	fifthteen()
+	for word in fifthteen:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	sixteen()
+	for word in sixteen:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	seventeen()
+	for word in seventeen:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	eightteen()
+	for word in eightteen:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	nineteen()
+	for word in nineteen:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	twenty()
+	for word in twenty:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	twentyone()
+	for word in twentyone:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	twentytwo()
+	for word in twentytwo:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	twentythree()
+	for word in twentythree:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	twentyfour()
+	for word in twentyfour:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	twentyfive()
+	for word in twentyfive:
+		f.write(word)
+		print(word)
+		f.write("\n")
+
+	twentysix()
+	for word in twentysix:
+		f.write(word)
+		print(word)
+		f.write("\n")
